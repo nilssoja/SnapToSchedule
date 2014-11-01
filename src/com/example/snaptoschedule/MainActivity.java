757ab9dc457bf8d0dc5f1c0983833a8a3acbc8c5
Jon Keilholz <keilhojk@gmail.com>
2014-11-01 14:55:36 -0400

    Wired up existing photo button
    existing photo button now opens gallery view and changes whatever image
    is selected to the image view on homescreen. This will be changed when
    OCR is implemented

--- src/com/example/snaptoschedule/MainActivity.java
+++ src/com/example/snaptoschedule/MainActivity.java
@@ -3,57 +3,109 @@ package com.example.snaptoschedule;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
-
 public class MainActivity extends Activity {
+
+	// start running m8s i swear onn me mum
+
+	private static final int RESULT_LOAD_IMAGE = 1;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		// http://viralpatel.net/blogs/pick-image-from-galary-android-app/
+		Button existingPhoto = (Button) findViewById(R.id.existing_button);
+		existingPhoto.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent i = new Intent(Intent.ACTION_PICK,
+						android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+				startActivityForResult(i, RESULT_LOAD_IMAGE);
+				
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		// int id = item.getItemId();
+		switch (item.getItemId()) {
+		case R.id.action_settings:
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+
+	public void startClassListTest(View v) {
+
+		// Place holder code.
+		// TODO: Add code for handling Content Provider data between Gallery and
+		// Camera Applications.
+		Context context = getApplicationContext();
+		CharSequence text = "In the future these buttons will take you to either the phones camera or gallery view.";
+		int duration = Toast.LENGTH_LONG;
+
+		Toast toast = Toast.makeText(context, text, duration);
+		toast.show();
+
+		Intent intent = new Intent(MainActivity.this, ClassListTest.class);
+		startActivity(intent);
+	}
+
 	
-	//start running m8s i swear onn me mum
+	public void startGalleryView() {
+		
+	}
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	// http://viralpatel.net/blogs/pick-image-from-galary-android-app/
+	//possibly redo using http://stackoverflow.com/questions/2507898/how-to-pick-an-image-from-gallery-sd-card-for-my-app
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
 
+		if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK
+				&& null != data) {
+			Uri selectedImage = data.getData();
+			String[] filePathColumn = { MediaStore.Images.Media.DATA };
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+			Cursor cursor = getContentResolver().query(selectedImage,
+					filePathColumn, null, null, null);
+			cursor.moveToFirst();
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        //int id = item.getItemId();
-        switch (item.getItemId()) {        
-        case R.id.action_settings:
-            return true;
-        default:
-            return super.onOptionsItemSelected(item);
-    }
-    }
-    
-    public void startClassListTest(View v) {
-    	
-    	//Place holder code.
-    	//TODO: Add code for handling Content Provider data between Gallery and Camera Applications.
-    	Context context = getApplicationContext();
-    	CharSequence text = "In the future these buttons will take you to either the phones camera or gallery view.";
-    	int duration = Toast.LENGTH_LONG;
+			int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+			String picturePath = cursor.getString(columnIndex);
+			cursor.close();
 
-    	Toast toast = Toast.makeText(context, text, duration);
-    	toast.show();
-    	
-        Intent intent = new Intent(MainActivity.this, ClassListTest.class);
-        startActivity(intent);
-    }
+			ImageView imageView = (ImageView) findViewById(R.id.imageView1);
+			imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+
+		}
+
+	}
+
 }
