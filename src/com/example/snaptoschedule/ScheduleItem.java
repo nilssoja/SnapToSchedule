358d8af7641bfa77aa2dde0b35b6695de43e332c
Jon Keilholz <keilhojk@gmail.com>
2014-11-08 14:57:10 -0500

    Created ScheduleItem class.
    Created ScheduleItem class.

---
+++ src/com/example/snaptoschedule/ScheduleItem.java
@@ -0,0 +1,80 @@
+package com.example.snaptoschedule;
+
+public class ScheduleItem {
+
+	public String grtype;
+	public String classFormat;
+	public String campus;
+	public String classTime;
+	public String building;
+	public String courseName;
+	
+	
+	public ScheduleItem() {
+		super();
+	}
+
+
+	public String getGrtype() {
+		return grtype;
+	}
+
+
+	public void setGrtype(String grtype) {
+		this.grtype = grtype;
+	}
+
+
+	public String getClassFormat() {
+		return classFormat;
+	}
+
+
+	public void setClassFormat(String classFormat) {
+		this.classFormat = classFormat;
+	}
+
+
+	public String getCampus() {
+		return campus;
+	}
+
+
+	public void setCampus(String campus) {
+		this.campus = campus;
+	}
+
+
+	public String getClassTime() {
+		return classTime;
+	}
+
+
+	public void setClassTime(String classTime) {
+		this.classTime = classTime;
+	}
+
+
+	public String getBuilding() {
+		return building;
+	}
+
+
+	public void setBuilding(String building) {
+		this.building = building;
+	}
+
+
+	public String getCourseName() {
+		return courseName;
+	}
+
+
+	public void setCourseName(String courseName) {
+		this.courseName = courseName;
+	}
+	
+	
+	
+	
+}
