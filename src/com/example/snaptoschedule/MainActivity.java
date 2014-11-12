d08698288578c4f03d3e6a37a13dee09d6ce3909
Justin Nilsson <nilssoja@mail.uc.edu>
2014-11-11 23:25:25 -0500

    Added OCR Functionality
    OCR now implemented for taking pictures as well as the gallary

--- src/com/example/snaptoschedule/MainActivity.java
+++ src/com/example/snaptoschedule/MainActivity.java
@@ -1,21 +1,38 @@
 package com.example.snaptoschedule;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.AssetManager;
 import android.database.Cursor;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
+import com.googlecode.tesseract.android.TessBaseAPI;
 
 public class MainActivity extends Activity {
+	
+	public static final String PACKAGE_NAME = "com.example.snaptoschedule";
+	public static final String DATA_PATH = Environment
+			.getExternalStorageDirectory().toString() + "/SnapToSchedule/";
+	public static final String lang = "eng";
+	private static final String TAG = "MainActivity.java";
 
 	// start running m8s i swear onn me mum
 
@@ -26,6 +43,52 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
+		String[] paths = new String[] { DATA_PATH, DATA_PATH + "tessdata/" };
+
+		for (String path : paths) {
+			File dir = new File(path);
+			if (!dir.exists()) {
+				if (!dir.mkdirs()) {
+					Log.v(TAG, "ERROR: Creation of directory " + path + " on sdcard failed");
+					return;
+				} else {
+					Log.v(TAG, "Created directory " + path + " on sdcard");
+				}
+			}
+
+		}
+		
+		// lang.traineddata file with the app (in assets folder)
+		// You can get them at:
+		// http://code.google.com/p/tesseract-ocr/downloads/list
+		// This area needs work and optimization
+		if (!(new File(DATA_PATH + "tessdata/" + lang + ".traineddata")).exists()) {
+			try {
+
+				AssetManager assetManager = getAssets();
+				InputStream in = assetManager.open("tessdata/" + lang + ".traineddata");
+				//GZIPInputStream gin = new GZIPInputStream(in);
+				OutputStream out = new FileOutputStream(DATA_PATH
+						+ "tessdata/" + lang + ".traineddata");
+
+				// Transfer bytes from in to out
+				byte[] buf = new byte[1024];
+				int len;
+				//while ((lenf = gin.read(buff)) > 0) {
+				while ((len = in.read(buf)) > 0) {
+					out.write(buf, 0, len);
+				}
+				in.close();
+				//gin.close();
+				out.close();
+				
+				Log.v(TAG, "Copied " + lang + " traineddata");
+			} catch (IOException e) {
+				Log.e(TAG, "Was unable to copy " + lang + " traineddata " + e.toString());
+			}
+		}
+		
+		
 		// http://viralpatel.net/blogs/pick-image-from-galary-android-app/
 		Button existingPhoto = (Button) findViewById(R.id.existing_button);
 		existingPhoto.setOnClickListener(new View.OnClickListener() {
@@ -35,6 +98,19 @@ public class MainActivity extends Activity {
 				Intent i = new Intent(Intent.ACTION_PICK,
 						android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
 				startActivityForResult(i, RESULT_LOAD_IMAGE);
+				
+			}
+		});
+		
+		Button newPhoto = (Button) findViewById(R.id.new_button);
+		newPhoto.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+			    if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+			        startActivityForResult(takePictureIntent, RESULT_LOAD_IMAGE);
+			    }
 				
 			}
 		});
@@ -99,7 +175,26 @@ public class MainActivity extends Activity {
 
 			int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
 			String picturePath = cursor.getString(columnIndex);
+			Bitmap bitmap = BitmapFactory.decodeFile(picturePath);
+			bitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true);
 			cursor.close();
+			
+			TessBaseAPI baseApi = new TessBaseAPI();
+			baseApi.setDebug(true);
+			baseApi.init(DATA_PATH, "eng");
+			baseApi.setImage(bitmap);
+			
+			String recognizedText = baseApi.getUTF8Text();
+			
+			Log.v(TAG, recognizedText);
+			
+			Context context = getApplicationContext();
+			int duration = Toast.LENGTH_LONG;
+
+			Toast toast = Toast.makeText(context, recognizedText, duration);
+			toast.show();
+
+			baseApi.end();
 
 			ImageView imageView = (ImageView) findViewById(R.id.imageView1);
 			imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
